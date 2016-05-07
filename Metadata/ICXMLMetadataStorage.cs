bbed1372389a8fb2441f14b5a088a5701886dbe5
George Birbilis <GitHub1@zoomicon.com>
2016-05-07 05:47:35 +0300

    - Changed project title & URL in header comments (now is autonomous from ClipFlair project) - At Diagrams solution folder keeping shortcut to class diagram and at filesystem folder keeping the exported PNG image file - Added ICXMLMetadataStorage interface - Updated class diagram and exported new diagram PNG image - Updated NuGet package to 1.0.1 and published

---
+++ Metadata/ICXMLMetadataStorage.cs
@@ -0,0 +1,14 @@
+﻿//Project: Metadata.CXML (https://github.com/zoomicon/Metadata.CXML)
+//Filename: ICXMLMetadataStorage
+//Version: 20160507
+
+using System.Collections.Generic;
+
+namespace Metadata.CXML
+{
+
+  public interface ICXMLMetadataStorage: IDictionary<string, ICXMLMetadata>
+  {
+  }
+
+}
